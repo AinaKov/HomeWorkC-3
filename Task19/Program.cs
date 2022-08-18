2b86ae7741e737d1e0f2e72044d468ee2a0dbada
AlinaKov <alkovkova@gmail.com>
2022-08-19 00:43:14 +0500

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// я так и не поняла: для чего мы пишем using System и class Program. У меня не работает с ними.
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <0)
+{
+    number = number * -1;
+}
+
+if (number > 9999 & number < 100000)
+{
+    if (number.ToString()[0] == number.ToString()[4] & number.ToString()[1] == number.ToString()[3])
+    {
+        Console.WriteLine("Палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Не пятизначное");
+}
